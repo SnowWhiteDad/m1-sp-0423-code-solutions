ebb7d1c68a031973144aaf97c241a6a41c72b6fa
Ola Sh2 <shittu.babs@gmail.com>
2023-04-07 08:53:40 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,37 @@
+const fullName = 'Ola Sh2';
+const isCool = true;
+const totalPets = 1;
+
+console.log(
+  'value of fullName is: ',
+  fullName,
+  '\n',
+  'value of isCool is: ',
+  isCool,
+  '\n',
+  'value of totalPets is: ',
+  totalPets,
+  '\n',
+  'typeof fullName: ',
+  typeof fullName,
+  '\n',
+  'typeof isCool: ',
+  typeof isCool,
+  '\n',
+  'typeof totalPets: ',
+  typeof totalPets
+);
+
+const unknown = null;
+
+console.log(
+  'value of unknown is: ',
+  unknown,
+  '\n',
+  'typeof unknown: ',
+  typeof unknown
+);
+
+let never;
+
+console.log('value of never is: ', never, '\n', 'typeof never: ', typeof never);
